5553cc8b98518cdb313a5fe21b631331cc32f13b
hn hn <2546515103@qq.com>
2023-04-09 01:42:00 +0800

    dark

--- redis-management/frontend/src/App.vue
+++ redis-management/frontend/src/App.vue
@@ -5,13 +5,16 @@ import Keys from "./components/Keys.vue";
 import {ref} from "vue";
 import {CreateValue, DbList, GetKeyValue, KeyList} from "../wailsjs/go/main/App.js";
 import KeyValue from "./components/KeyValue.vue";
-
+import { useDark, useToggle } from '@vueuse/core'
 let updateFlag=ref(true)
 let keyValue=ref()
 let keyList=ref()
 let keyConnId=ref()
 let keyDb=ref()
 let keyKey = ref()
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 function updateConnection(){
   updateFlag.value=!updateFlag.value
 }
@@ -39,10 +42,13 @@ function selectKey(key) {
 
 <template>
   <el-row>
-    <el-col :span="6" style="height: 100vh;padding:15px;">
+    <button @click="toggleDark()">
+      当前状态是: {{ isDark }}
+    </button>
+    <el-col :span="6" style="height: 100vh;padding:15px;" >
       <div style="margin-bottom:12px" >
         <ConnectionManage title="新建连接" btn-type="primary" @emit-connection-list="updateConnection"/>
-<!--        <el-button @click="addKeyValue()">测试新增</el-button>-->
+        <!--        <el-button @click="addKeyValue()">测试新增</el-button>-->
       </div>
       <ConnectionList @emit-select-db="selectDB" :update="updateFlag"/>
     </el-col>
@@ -50,12 +56,11 @@ function selectKey(key) {
       <Keys :keyDb="keyDb" :keyConnId="keyConnId" @emit-select-key="selectKey" />
     </el-col>
     <el-col :span="8" style="padding: 20px">
-<!--      dbList==>{{keyList}}-->
+      <!--      dbList==>{{keyList}}-->
       <KeyValue :keyDb="keyDb" :keyConnId="keyConnId" :keyKey="keyKey"/>
     </el-col>
   </el-row>
 </template>
 
 <style>
-
 </style>
--- redis-management/main.go
+++ redis-management/main.go
@@ -22,8 +22,10 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 1},
-		OnStartup:        app.startup,
+		BackgroundColour: &options.RGBA{
+			255, 255, 255, 255,
+		},
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
